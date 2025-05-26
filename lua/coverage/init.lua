d4ef01b141d6fe94ea727108fafbe47b3272bc5f
Manuel Zambrano <mzambranor@caixabanktech.com>
2025-05-26 15:01:11 +0200

    change require path

--- lua/coverage/init.lua
+++ lua/coverage/init.lua
@@ -5,27 +5,27 @@ function M.setup()
 	vim.api.nvim_create_autocmd("BufEnter", {
 		pattern = "*.tsx, *.ts, *.jsx, *.js",
 		callback = function()
-			vim.cmd("silent! lua require('coverage.coverage').apply_coverage_highlights()")
+			vim.cmd("silent! lua require('coverage').apply_coverage_highlights()")
 		end,
 	})
 	vim.api.nvim_create_autocmd("BufWritePost", {
 		pattern = "lcov.info",
 		callback = function()
-			vim.cmd("silent! lua require('coverage.coverage').apply_coverage_highlights()")
+			vim.cmd("silent! lua require('coverage').apply_coverage_highlights()")
 		end,
 	})
 
 	vim.api.nvim_create_user_command("CoverageRefresh", function()
-		require("coverage.coverage").apply_coverage_highlights()
+		require("coverage").apply_coverage_highlights()
 	end, {})
 
 	vim.keymap.set("n", "<leader>tcs", function()
-		require("coverage.coverage").show_coverage_summary()
+		require("coverage").show_coverage_summary()
 	end, { desc = "Test Coverage Summary" })
 
 	vim.keymap.set("n", "<leader>tcr", "<cmd>CoverageRefresh<CR>", { desc = "Coverage refresh highlights" })
 	vim.keymap.set("n", "<leader>tct", function()
-		require("coverage.coverage").toggle_coverage_highlights()
+		require("coverage").toggle_coverage_highlights()
 	end, { desc = "Coverage toggle highlights" })
 end
 
